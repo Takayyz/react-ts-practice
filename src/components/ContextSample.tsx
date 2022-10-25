37c8e20a82c07d8f6d7c707011ab1f0ffb7236a1
Takayyz <like.the.wind0306@gmail.com>
2022-10-26 02:50:47 +0900

    feat: Context Provider,Comsumer実装

---
+++ src/components/ContextSample.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const TitleContext = React.createContext('');
+
+const Title = () => {
+  return (
+    <TitleContext.Consumer>
+      {(title) => <h1>{title}</h1>}
+    </TitleContext.Consumer>
+  );
+};
+
+const Header = () => {
+  return (<div><Title /></div>);
+};
+
+const Page = () => {
+  const title = 'React Book';
+
+  return (
+    <TitleContext.Provider value={title}>
+      <Header />
+    </TitleContext.Provider>
+  );
+};
+export default Page;
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import Hello from './components/Hello';
 import Name from './components/Name';
 import Message from './components/Message';
 import ContainerSample from './components/ContainerSample';
+import ContextSample from './components/ContextSample';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,6 +18,7 @@ root.render(
     <Name />
     <Message />
     <ContainerSample />
+    <ContextSample />
   </React.StrictMode>
 );
 
